5fc1e65f8dcfabc85c30038f00d5fd9db9422ead
muhammadalimurtadho05 <muhammadalimurtadho2209051@gmail.com>
2026-06-10 21:35:59 +0700

    fix: logo himatif

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,14 +5,10 @@
 >
   <!-- Logo -->
   <div class="flex items-center gap-3 px-5 py-5 border-b border-slate-200">
-    <div class="w-9 h-9 rounded-xl bg-blue-600 flex items-center justify-center flex-shrink-0">
-      <svg class="w-5 h-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
-      </svg>
-    </div>
+    <img src="{{ asset('images/logo.png') }}" width="50px">
     <div class="sidebar-logo-text transition-all duration-300">
-      <div class="font-display font-800 text-slate-900 text-base leading-tight">SiAbsen</div>
-      <div class="text-slate-500 text-xs">v2.0 Digital</div>
+      <div class="font-display font-800 text-slate-900 text-base leading-tight">Absensi</div>
+      <div class="text-slate-500 text-xs">HIMATIF 2026</div>
     </div>
   </div>
 
